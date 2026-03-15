c5242ca8704628991334a912cb6a63812cf75828
AlejoHerrer <alejandroherreralbengrin@gmail.com>
2026-03-15 13:15:13 -0500

    Ejercicio 1

---
+++ ejercicio1.php
@@ -0,0 +1,14 @@
+<?php
+
+    //Esto es un comentario de una linea
+
+    //Estas son formas de imprimir en pantalla
+    echo "Hola mundo <br>";
+    print_r("Hola mundo plus <br>");
+    print("Hola mundo plus 2");
+
+    /*
+         Este es un comentario en bloque
+    */
+
+?>
