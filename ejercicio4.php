67fbc6c14a2de73b8d6c0860143adf75b51a70ab
AlejoHerrer <alejandroherreralbengrin@gmail.com>
2026-03-15 22:30:22 -0500

    Ejercicio 3, 4 y 5

---
+++ ejercicio4.php
@@ -0,0 +1,10 @@
+<?php
+
+    $Edad = 26;
+    $Nombre = "Alejandro";
+    $Altura = 1.4;
+    $Puesto = null;
+
+    echo "Hola ".$Nombre.", tienes ".$Edad." y mides ".$Altura." metros. <br>Tu puesto es ".$Puesto;
+    
+?>
---
+++ ejercicio5.php
@@ -0,0 +1,12 @@
+<?php
+
+    //Define una constante
+    const NOMBRE = "ALEJANDRO";
+
+    //Otra forma de definir una constante
+    define('EDAD',26);
+
+    echo "Mi nombre es ".NOMBRE." y tengo ".EDAD." años.";
+
+
+?>
